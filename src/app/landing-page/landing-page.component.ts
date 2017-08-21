88446d11046d2f92d6e63c8619695f46afcac248
Ashwin Thakre <ashwin@rapidlo.com>
2017-08-21 11:30:45 +0530

    typed.js added and tested in landing-page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as Typed from 'typed.js';
 
 @Component({
   selector: 'app-landing-page',
@@ -13,8 +13,31 @@ export class LandingPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    $(document).ready(function(){
-      $('.parallax').parallax();
+    $('.parallax').parallax();
+
+    const typed = new Typed('.type', {
+      strings: ['neighbor', 'family', 'team', 'community'],
+      stringsElement: null,
+      // typing speed
+      typeSpeed: 60,
+      // time before typing starts
+      startDelay: 600,
+      // backspacing speed
+      backSpeed: 20,
+      // time before backspacing
+      backDelay: 500,
+      // loop
+      loop: true,
+      // false = infinite
+      loopCount: Infinity,
+      // show cursor
+      showCursor: false,
+      // character for cursor
+      cursorChar: '|',
+      // attribute to type (null == text)
+      attr: null,
+      // either html or text
+      contentType: 'html'
     });
   }
 }
