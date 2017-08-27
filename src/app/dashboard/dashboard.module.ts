fa9d0167bbb9c8ff7a105fe648a0ec5b72968775
Ashwin Thakre <ashwin@rapidlo.com>
2017-08-27 23:27:34 +0530

    custom element schema added in dashboard module ts

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import{DashboardRoutingModule} from './dashboard-routing.module';
+import {DashboardRoutingModule} from './dashboard-routing.module';
 
-import { DashboardComponent }   from './dashboard.component';
+import { DashboardComponent } from './dashboard.component';
 import {HeroSearchComponent} from './hero-search/hero-search.component';
 import {HeroService} from '../shared/services/hero.service';
+
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   declarations: [
     DashboardComponent,
