63610fc6defee2c547e3a1c540e53defbb849541
Ashwin Thakre <ashwin@rapidlo.com>
2017-08-28 23:01:36 +0530

    get-started added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
-  { path: 'dashboard',  loadChildren: 'app/dashboard/dashboard.module#DashboardModule'},
+  { path: 'dashboard', loadChildren: 'app/dashboard/dashboard.module#DashboardModule'},
   { path: 'detail/:id', loadChildren: 'app/shared/components/hero-detail/hero-detail.module#HeroDetailModule' },
-  { path: 'heroes',     loadChildren: 'app/heroes/heroes.module#HeroesModule'},
-  { path: 'landing-page',     loadChildren: 'app/landing-page/landing-page.module#LandingPageModule'}
+  { path: 'heroes', loadChildren: 'app/heroes/heroes.module#HeroesModule'},
+  { path: 'landing-page', loadChildren: 'app/landing-page/landing-page.module#LandingPageModule'},
+  { path: 'get-started', loadChildren: 'app/get-started/get-started.module#GetStartedModule'}
 ];
 
 @NgModule({
