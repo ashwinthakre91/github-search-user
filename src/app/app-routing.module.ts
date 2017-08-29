1c59661d0cb4d378fa188ab97a778fc303c8959c
Ashwin Thakre <ashwin@rapidlo.com>
2017-08-29 23:13:16 +0530

    app routing module updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: 'dashboard', loadChildren: 'app/dashboard/dashboard.module#DashboardModule'},
   { path: 'detail/:id', loadChildren: 'app/shared/components/hero-detail/hero-detail.module#HeroDetailModule' },
   { path: 'heroes', loadChildren: 'app/heroes/heroes.module#HeroesModule'},
-  { path: 'landing-page', loadChildren: 'app/landing-page/landing-page.module#LandingPageModule'},
+  { path: '', loadChildren: 'app/landing-page/landing-page.module#LandingPageModule'},
   { path: 'get-started', loadChildren: 'app/get-started/get-started.module#GetStartedModule'},
   { path: 'sign-in', loadChildren: 'app/sign-in/sign-in.module#SignInModule'}
 ];
